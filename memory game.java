0b8402223b0c3cc6031c5c909101fbea794601e4
Tanushree Borase <130696335+TanushreeBorase@users.noreply.github.com>
2023-06-16 11:20:51 +0530

    Add files via upload

---
+++ memory game.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class MemoryGame {
+
+	public static void main(String[] args)
+	{
+
+		Scanner scanner = new Scanner(System.in);
+		ArrayList<String> cards = new ArrayList<>();
+		cards.add("A");
