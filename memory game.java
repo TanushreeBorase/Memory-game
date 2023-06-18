f55e9fe104736cc8f5b285056ae76d0b13933bfd
Tanushree Borase <130696335+TanushreeBorase@users.noreply.github.com>
2023-06-18 08:50:01 +0530

    Update memory game.java

--- memory game.java
+++ memory game.java
@@ -76,4 +76,21 @@ public class MemoryGame
 								boolean[] flipped,
 								String prompt)
 	{
-		
+		int index;
+		while (true) {
+			System.out.println(prompt);
+			index = scanner.nextInt();
+			if (index < 0 || index >= board.length) {
+				System.out.println(
+					"Invalid index, try again.");
+			}
+		else if (flipped[index]) {
+				System.out.println(
+					"Card already flipped, try again.");
+			}
+			else {
+				break;
+			}
+		}
+		return index;
+	}
